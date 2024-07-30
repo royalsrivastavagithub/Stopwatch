119346b156224a762223e5922421dce0b6dd3d11
royalsrivastavagithub <royalsrivastavaofficial@gmail.com>
2024-07-30 18:09:05 +0530

    Added functionality to start/stop and reset with keyborad keys.

--- src/App.jsx
+++ src/App.jsx
@@ -19,9 +19,22 @@ export default function App() {
     return () => clearInterval(interval.current);
   }, [isRunning,currentTime]);
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.code === 'Space') {
+        handleStartStop();
+      } else if (event.code === 'KeyR') {
+        handleReset();
+      }
+    };
+  
+    document.addEventListener('keydown', handleKeyPress);
+    return () => document.removeEventListener('keydown', handleKeyPress);
+  });
+
 
 //this triggers when START/STOP button is pressed
-function handleStarStop(){
+function handleStartStop(){
   setisRunning(!isRunning);
   console.log("START/STOP button pressed.")
 }
@@ -31,7 +44,10 @@ function handleReset(){
   setCurrentTime(0);
   setisRunning(false);
   clearInterval(interval.current);
+
 }    
+
+
 function format(millisec) {
   const milliseconds = Math.floor(millisec % 1000);
   const seconds = Math.floor(millisec / 1000) % 60; 
@@ -47,11 +63,14 @@ function format(millisec) {
 
   return (
     <>
-      <div className="bg-black h-screen text-white">
-        <div id="display">{format(currentTime)}</div>
-        <div id="buttons">
-          <button onClick={handleStarStop}>{isRunning?"STOP":"START"}</button>
-          <button onClick={handleReset}>RESET</button>
+      <div className="bg-zinc-800 h-screen text-white">
+        <div className='bg-slate-600 border-8'><h1 className='text-center'>STOP WATCH</h1></div>
+        <div id="display"><h1 className='text-9xl text-center'>{format(currentTime)}</h1></div>
+        <div id="buttons" className='flex flex-row gap-96'>
+          <div></div>
+          <button onClick={handleStartStop} className='bg-slate-500'>{isRunning?"STOP":"START"}</button>
+          <button onClick={handleReset} className='bg-red-600'>RESET</button>
+          <div></div>
         </div>
         </div>
     </>
